c72d60a7753fb00658ce32107fc670880c3150d5
Misiaty-Mat <m.a.s.m@interia.pl>
2021-10-14 10:38:54 +0200

    fixing the code and adding comments

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-
 import pandas
 
+# importing data from csv
 alphabet_data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
 
+# changing data from DataFrame to dictionary
+alphabet_dict = {row.letter: row.code for (
+    index, row) in alphabet_data_frame.iterrows()}
 
-alphabet_dict = {}
-
-for (index, row) in alphabet_data_frame.iterrows():
-    alphabet_dict[row.letter] = row.code
-
+# asking user for input
 user_input = input("Enter a word: ").upper()
 
+
+# looking for word inside dictionary for each letter from user input
 NATO_alphapet_for_word = [alphabet_dict[letter] for letter in user_input]
 
+
+# printing result
 for word in NATO_alphapet_for_word:
-    print(word, end=', ')
+    print(word, end='')
+    if word != NATO_alphapet_for_word[-1]:
+        print(', ', end='')
