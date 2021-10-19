ae5c2ac69941dece915042a3a398c654fbdeff81
Misiaty-Mat <m.a.s.m@interia.pl>
2021-10-19 16:50:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,23 @@ alphabet_data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabet_dict = {row.letter: row.code for (
     index, row) in alphabet_data_frame.iterrows()}
 
-# asking user for input
-user_input = input("Enter a word: ").upper()
 
-
-# looking for word inside dictionary for each letter from user input
-NATO_alphapet_for_word = [alphabet_dict[letter] for letter in user_input]
-
+wrong_word_format = True
+while wrong_word_format:
+    # asking user for input
+    user_input = input("Enter a word: ").upper()
+    try:
+        # looking for word inside dictionary for each letter from user input
+        NATO_alphapet_for_word = [alphabet_dict[letter]
+                                  for letter in user_input]
+    # if user entered wrong formated word
+    except KeyError:
+        print("Sorry only letters in one word are alowed")
+    else:
+        wrong_word_format = False
 
 # printing result
-for word in NATO_alphapet_for_word:
+for index, word in enumerate(NATO_alphapet_for_word):
     print(word, end='')
-    if word != NATO_alphapet_for_word[-1]:
+    if index != len(NATO_alphapet_for_word) - 1:
         print(', ', end='')
